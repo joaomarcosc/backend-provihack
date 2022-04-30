7be7b8e2caade34140c5727dd37d1d0a06194a50
Renan Lage <renanlopresti@gmail.com>
2022-04-30 13:04:53 -0300

    port

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { AddressInfo } from "net";
 import { discartRouter, establishmentRouter } from "./controller/routes/establishmentRouter";
 
+const PORT = process.env.PORT
+
 const app = express();
 
 app.use(express.json());
@@ -9,7 +11,7 @@ app.use(express.json());
 app.use("/establishment", establishmentRouter);
 app.use("/discart", discartRouter);
 
-const server = app.listen(3003, () => {
+const server = app.listen(PORT, () => {
  if (server) {
   const address = server.address() as AddressInfo;
   console.log(`Servidor rodando em http://localhost:${address.port}`);
